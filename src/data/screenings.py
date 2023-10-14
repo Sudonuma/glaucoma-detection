d2f4f18674665d4d4f3323c48783737a31351eff
Sudonuma <tasnim.hd@gmail.com>
2023-10-14 19:40:27 +0200

    add docstring and type hinting to the dataset class

--- src/data/screenings.py
+++ src/data/screenings.py
@@ -1,55 +1,66 @@
-import numpy as np
-import random
 from PIL import Image
-
-from torch.utils.data import Dataset
-import torch
-import os
-import glob
 import pandas as pd
+from torch.utils.data import Dataset
+import glob
 
-import torch
-from torch.utils.data import DataLoader
-from torchvision import transforms
 
-def pil_loader(path):
+def pil_loader(path: str) -> Image:
+    """
+    Load an image from the specified path.
+
+    Args:
+        path (str): The file path to the image.
+
+    Returns:
+        Image: A PIL Image in RGB format.
+    """
     with open(path, 'rb') as f:
         with Image.open(f) as img:
             return img.convert('RGB')
 
 class ResnetDataset(Dataset):
-    def __init__(self, root_dir, csv_file, transform=None):
+    def __init__(self, root_dir: str, csv_file: str, transform=None):
+        """
+        Initialize a custom dataset for ResNet.
+
+        Args:
+            root_dir (str): The root directory where the image files are located.
+            csv_file (str): The path to a CSV file containing image file names and labels.
+            transform (callable, optional): A function/transform to apply to the images.
+
+        Returns:
+            None
+        """
         self.root_dir = root_dir
         self.df = pd.read_csv(csv_file)
         self.transform = transform
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """
+        Get the number of samples in the dataset.
+
+        Returns:
+            int: The number of samples in the dataset.
+        """
         return len(self.df)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> tuple:
+        """
+        Get an item from the dataset.
+
+        Args:
+            idx (int): The index of the item to retrieve.
+
+        Returns:
+            tuple: A tuple containing the image and label.
+        """
         img_name = self.df.iloc[idx, 0]
         file_path = glob.glob(f'{self.root_dir}/**/{img_name}.jpg', recursive=True)
         
         image = pil_loader(file_path[0])
-        label = self.df.iloc[idx, 1]  # The label encoded as an integer (remove the class from the trainer)
+        label = self.df.iloc[idx, 1]
 
         if self.transform:
             image = self.transform(image)
 
         return image, label
-    
-#  for testing (primarly)
-if __name__ == "__main__":
-    # transformations
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])  
-    resnet_dataset = ResnetDataset(root_dir='/home/sudonuma/Documents/glaucoma/data/dataset', csv_file='dataset/train_labels.csv', transform=transform)
-
-    
-    data_loader = DataLoader(resnet_dataset, batch_size=1, shuffle=True)
-
-    for batch in data_loader:
-        image, label = batch
