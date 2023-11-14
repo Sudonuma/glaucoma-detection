b060763c7a632520941a04afe5ca88b9658ab4ec
Sudonuma <tasnim.hd@gmail.com>
2023-11-14 22:40:22 +0100

    fix download data path

--- main.py
+++ main.py
@@ -1,81 +1,30 @@
-import logging
 import os
 
-import wandb
-from src.download_script import download_data, download_model
-from src.options import NetworkOptions
-from src.train import train
-from src.trainer import infer_model
-from src.validate_model import validate
+import hydra
+import mlflow
+from omegaconf import DictConfig
 
 
-def main():
-    # initialize wandb
-    wandb.init(project="glaucoma", entity="sudonuma")
+# This automatically reads in the configuration
+@hydra.main(config_name="config")
+def go(config: DictConfig):
 
-    # setup the logger
-    log_dir = "./logs"
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
+    # Setup the wandb experiment. All runs will be grouped under this name
+    os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
+    os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
 
-    log_formatter = logging.Formatter(
-        "%(asctime)s, %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+    # You can get the path at the root of the MLflow project with this:
+    root_path = hydra.utils.get_original_cwd()
+
+    _ = mlflow.run(
+        os.path.join(root_path, "components/download_script"),
+        "main",
+        parameters={
+            "bucket_name": config["data"]["bucket_name"],
+            "source_blob_name": config["data"]["source_blob_name"],
+        },
     )
-
-    # Setup File handler
-    file_handler = logging.FileHandler("./logs/logs.log")
-    file_handler.setFormatter(log_formatter)
-    file_handler.setLevel(logging.INFO)
-
-    # Setup Stream Handler
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(log_formatter)
-    stream_handler.setLevel(logging.INFO)
-
-    # Get our logger
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-
-    # Add both Handlers
-    logger.addHandler(file_handler)
-    logger.addHandler(stream_handler)
-
-    # parse options
-    options = NetworkOptions()
-    opts = options.parse()
-
-    if opts.validate_only:
-        # Download or not model
-        logger.info("Validation and Inference only mode:")
-        user_input = (
-            input(
-                "Do you want to download a different model. (your current model will be DELETED) (yes/no)? "
-            )
-            .strip()
-            .lower()
-        )
-        if user_input == "yes":
-            download_model(opts.model_url, logger)
-
-        # Evaluate model
-        validate(opts, logger)
-
-        # Infer model
-        infer_model(opts, logger)
-        return
-
-    # Download data
-    download_data(opts.data_url, logger)
-
-    # Train model
-    train(opts, logger)
-
-    # Evaluate model
-    validate(opts, logger)
-
-    # Infer model
-    infer_model(opts, logger)
 
 
 if __name__ == "__main__":
-    main()
+    go()
---
+++ run.py
@@ -0,0 +1,81 @@
+import logging
+import os
+
+import wandb
+from src.download_script import download_data, download_model
+from src.options import NetworkOptions
+from src.train import train
+from src.trainer import infer_model
+from src.validate_model import validate
+
+
+def main():
+    # initialize wandb
+    wandb.init(project="glaucoma", entity="sudonuma")
+
+    # setup the logger
+    log_dir = "./logs"
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    log_formatter = logging.Formatter(
+        "%(asctime)s, %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+    # Setup File handler
+    file_handler = logging.FileHandler("./logs/logs.log")
+    file_handler.setFormatter(log_formatter)
+    file_handler.setLevel(logging.INFO)
+
+    # Setup Stream Handler
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(log_formatter)
+    stream_handler.setLevel(logging.INFO)
+
+    # Get our logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    # Add both Handlers
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+    # parse options
+    options = NetworkOptions()
+    opts = options.parse()
+
+    if opts.validate_only:
+        # Download or not model
+        logger.info("Validation and Inference only mode:")
+        user_input = (
+            input(
+                "Do you want to download a different model. (your current model will be DELETED) (yes/no)? "
+            )
+            .strip()
+            .lower()
+        )
+        if user_input == "yes":
+            download_model(opts.model_url, logger)
+
+        # Evaluate model
+        validate(opts, logger)
+
+        # Infer model
+        infer_model(opts, logger)
+        return
+
+    # Download data
+    download_data(opts.data_url, logger)
+
+    # Train model
+    train(opts, logger)
+
+    # Evaluate model
+    validate(opts, logger)
+
+    # Infer model
+    infer_model(opts, logger)
+
+
+if __name__ == "__main__":
+    main()
